88a85ffdecc048f2e8a12fe21114855b9373e414
Tangobyte <112774024+Tangobyte@users.noreply.github.com>
2023-12-11 12:35:48 +0100

    addedfetchmodule

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ const express = require('express')
 const {ROUTES} = require("./routes");
 const {setupLogging} = require("./logging");
 const {setupProxies} = require("./proxy");
+const supabaseService = require('./supabaseService');
+const cors = require('cors'); 
 
-// Environment path
-import * as dotenv from 'dotenv';
-dotenv.config({ path: 'variables.env' });
+// // Environment path
+// import * as dotenv from 'dotenv';
+// dotenv.config({ path: 'variables.env' });
 
 const app = express()
 const port = 3000;
+
+app.use(cors());
+
+app.get('/api/data', async (req, res) => {
+    try {
+      const data = await supabaseService.fetchDataFromSupabase();
+      res.json(data);
+    } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 setupLogging(app);
 setupProxies(app, ROUTES);  
--- supabaseService.js
+++ supabaseService.js
@@ -1,11 +1,11 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const fetchDataFromSupabase = async () => {
   try {
-    const supabaseEndpoint = 'https://YOUR_SUPABASE_ENDPOINT.supabase.co';
-    const supabaseApiKey = 'YOUR_SUPABASE_API_KEY';
+    const supabaseEndpoint = 'https://ebeuycfrffrormwdrcnm.supabase.co';
+    const supabaseApiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZXV5Y2ZyZmZyb3Jtd2RyY25tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA2NDQwMzgsImV4cCI6MjAxNjIyMDAzOH0.kNbKs9c0aDFFaeDOE2iP2KzI26LmWtB4VGcBD-zEmK8';
 
-    const response = await fetch(`${supabaseEndpoint}/rest/v1/YOUR_TABLE_NAME`, {
+    const response = await fetch(`${supabaseEndpoint}/rest/v1/calendar`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
