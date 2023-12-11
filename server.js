c4bb10690dbe76bae7c76c7bd1e3134f957e9ab6
Tangobyte <112774024+Tangobyte@users.noreply.github.com>
2023-12-11 09:58:04 +0100

    addedenv

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const {ROUTES} = require("./routes");
 const {setupLogging} = require("./logging");
 const {setupProxies} = require("./proxy");
 
+// Environment path
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'variables.env' });
+
 const app = express()
 const port = 3000;
 
