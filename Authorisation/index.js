6c2dc51ec80eec9066c97d6761a3144a55c35f53
Tangobyte <112774024+Tangobyte@users.noreply.github.com>
2023-12-18 22:50:44 +0100

    updated package.json

---
+++ Authorisation/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); 
+const port = process.env.PORT || 3003;
+app.use(cors());
+
+const routes = require('./routes');
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
