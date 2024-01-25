f2cc38980bc909c64b5ec7df82bc300a5db1531e
lewie zhu <zhu0027@hz.nl>
2024-01-25 11:42:23 +0100

    Backup commit

--- Calendar/calendar.test.js
+++ Calendar/calendar.test.js
@@ -7,13 +7,13 @@ describe('Add Event (POST Request)', () => {
       start: '2024-02-01T12:00:00Z',
     };
 
-    const response = await axios.post('http://localhost:3000/events', newEvent);
+    const response = await axios.post('http://localhost:3002/events', newEvent);
 
     //Check the response status code is 201
     expect(response.status).toBe(201);
 
     //Verify that the event is added to the database by fetching and comparing.
-    const fetchedEventResponse = await axios.get(`http://localhost:3000/events/${response.data.id}`);
+    const fetchedEventResponse = await axios.get(`http://localhost:3002/events/${responseQ.data.id}`);
 
     //Check if the fetched event matches the added event
     expect(fetchedEventResponse.data).toEqual(newEvent);
